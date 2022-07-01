5f10a106af1210f862338c58abb4c07bcb9342e8
luiseliasdavid <luis.elias.david@gmail.com>
2022-07-01 00:54:35 -0300

    card boostrap

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import {Routes, Route } from 'react-router-dom'
- import Login from "./components/Login";
- import Listado from './components/Listado'
+ import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Login from "./components/Login";
+import Listado from './components/Listado'
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 
 function App() {
   return (
-    <>
+    <div className='container'>
+    <Header/>
+  
     <Routes>
       <Route exact path='/' element={<Login/>} />
       <Route path='/listado' element={<Listado/>} />
      </Routes>
-    </>
+     
+     <Footer/>
+     </div>
   );
 }
 
--- src/components/Listado.js
+++ src/components/Listado.js
@@ -1,6 +1,8 @@
 import {useNavigate} from 'react-router-dom';
 import { useEffect} from 'react';
+import { Card,Button } from 'react-bootstrap';
 
+console.log(Card) 
 
 export default function Listado (){
     const navigate= useNavigate()
@@ -9,12 +11,25 @@ export default function Listado (){
         const token= localStorage.getItem('token')
        if(token===null){
          navigate('/')}
-    },[])
+    },[navigate])
     
-    
-    
-
     return (
-    <h2>componente listado</h2>
+    <div className='row'>
+
+    <div className='col-3' >
+    <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="holder.js/100px180" />
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
+    
+</div>
+    </div>
     )
 }
