161b7aa5007b5381287a94a01f07db4d4655d80f
luiseliasdavid <luis.elias.david@gmail.com>
2022-08-18 15:37:40 -0300

    Contact

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import "bootstrap-icons/font/bootstrap-icons.css"
 
 import App from './App';
 import { store } from './app/store'
@@ -8,6 +9,7 @@ import { Provider } from 'react-redux'
 
 
 import { BrowserRouter } from 'react-router-dom';
+import { Form } from 'react-bootstrap';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
